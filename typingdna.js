b1341d6d39c912e0eca01e8fd524dbcff2ea3a12
Raul <raul@typingdna.com>
2017-02-03 19:33:05 +0200

    typingdna.js class, version 1.2 created on Apr 8, 2016

---
+++ typingdna.js
@@ -0,0 +1,394 @@
+/**
+ * TypingDNA - Typing Biometrics JavaScript API v1.2
+ * http://typingdna.com/scripts/typingdna.js
+ * Use your own copy. (hotlinks not recommended)
+ *
+ * @version 1.2
+ * @author Raul Popa
+ * @copyright Typingdna.com
+ * @license http://www.apache.org/licenses/LICENSE-2.0
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Typical usage:
+ * var tdna = new TypingDNA(); // creates a new TypingDNA object and starts recording
+ * var typingPattern = tdna.get(); // returns a typing pattern (and continues recording afterwards),
+ * optionally you can pass a length, tdna.get(200) will return the pattern based on the last 200 key events.
+ *
+ * Optional:
+ * tdna.stop(); // ends recording and clears history stack (returns recording flag: false)
+ * tdna.start(); // restarts the recording after a stop (returns recording flag: true)
+ * tdna.reset(); // restarts the recording anytime, clears history stack and starts from scratch (returns nothing)
+ * var typingPatternQuality = TypingDNA.getQuality(typingPattern); //returns the quality/strength of any typing pattern
+ * (there is no need to initialize the class to do pattern quality checking)
+ */
+
+/**
+ * Creates a single instance (or a reference) of the TypingDNA class
+ * @param  {Number} maxHistoryLength Optional: The maximum length of the
+ * history stack, default:2000; optimal lengths needed for good quality
+ * typing patterns are between 500 and 5000, you can also pass 0 for default.
+ * @return {Object} Returns the single instance of the TypingDNA class.
+ * @example var tdna = new TypingDNA();
+ * @example var tdna = new TypingDNA(0);
+ * @example var tdna = new TypingDNA(300);
+ */
+function TypingDNA(maxHistoryLength) {
+  if (TypingDNA.initialized != true) {
+    TypingDNA.prototype.start = function() {
+      return TypingDNA.start.apply(this, arguments);
+    }
+    TypingDNA.prototype.stop = function() {
+      return TypingDNA.stop.apply(this, arguments);
+    }
+    TypingDNA.prototype.reset = function() {
+      return TypingDNA.reset.apply(this, arguments);
+    }
+    TypingDNA.prototype.get = function(args) {
+      return TypingDNA.get.apply(this, arguments);
+    }
+    TypingDNA.prototype.getQuality = function(args) {
+      return TypingDNA.getQuality.apply(this, arguments);
+    }
+    TypingDNA.prototype.maxHistoryLength = TypingDNA.maxHistoryLength;
+    TypingDNA.prototype.defaultHistoryLength = TypingDNA.defaultHistoryLength;
+    TypingDNA.prototype.maxSeekTime = TypingDNA.maxSeekTime;
+    TypingDNA.prototype.maxPressTime = TypingDNA.maxPressTime;
+    TypingDNA.instance = this;
+    TypingDNA.element = document;
+    TypingDNA.maxHistoryLength = 2000;
+    TypingDNA.maxSeekTime = 1500;
+    TypingDNA.maxPressTime = 300;
+    TypingDNA.defaultHistoryLength = 500;
+    // 44 chars
+    TypingDNA.chars = [97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 32, 39, 44, 46, 59, 63, 45, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57];
+    TypingDNA.pt1 = TypingDNA.ut1 = (new Date).getTime();
+    TypingDNA.wfk = [];
+    TypingDNA.sti = [];
+    TypingDNA.skt = [];
+    TypingDNA.rkc = [];
+    TypingDNA.lastDownKey;
+    TypingDNA.prevKeyCode = 0;
+    TypingDNA.zl = 0.000000000001;
+    TypingDNA.keydown = function(e) {
+      TypingDNA.lastDownKey = e.which;
+    }
+    TypingDNA.keypress = function(e) {
+      var t0 = TypingDNA.pt1;
+      TypingDNA.pt1 = (new Date).getTime();
+      var rkco = e.which;
+      var seekTotal = TypingDNA.pt1 - t0;
+      var startTime = TypingDNA.pt1;
+      var downKey = TypingDNA.lastDownKey;
+      TypingDNA.wfk[downKey] = 1;
+      TypingDNA.skt[downKey] = seekTotal;
+      TypingDNA.sti[downKey] = startTime;
+      TypingDNA.rkc[downKey] = rkco;
+    }
+    TypingDNA.keyup = function(e) {
+      var ut = (new Date).getTime();
+      var upKey = e.which;
+      if (TypingDNA.wfk[upKey] == 1) {
+        var keyCode = TypingDNA.rkc[upKey];
+        var pressTime = ut - TypingDNA.sti[upKey];
+        var seekTime = TypingDNA.skt[upKey];
+        var arr = [keyCode, seekTime, pressTime, TypingDNA.prevKeyCode, ut];
+        TypingDNA.history.add(arr);
+        TypingDNA.prevKeyCode = keyCode;
+        TypingDNA.wfk[upKey] = 0;
+      }
+    }
+    TypingDNA.element.addEventListener("keydown", TypingDNA.keydown);
+    TypingDNA.element.addEventListener("keypress", TypingDNA.keypress);
+    TypingDNA.element.addEventListener("keyup", TypingDNA.keyup);
+
+    /**
+     * Resets the history stack
+     */
+    TypingDNA.reset = function() {
+      TypingDNA.history.stack = [];
+    }
+
+    /**
+     * Automatically called at initilization. It starts the recording of keystrokes.
+     */
+    TypingDNA.start = function() {
+      if (TypingDNA.recording == true) {
+        TypingDNA.stop();
+      }
+      TypingDNA.reset();
+      return TypingDNA.recording = true;
+    }
+
+    /**
+     * Ends the recording of further keystrokes. To restart recording afterwards you can
+     * either call TypingDNA.start() or create a new TypingDNA object again, not recommended.
+     */
+    TypingDNA.stop = function() {
+      TypingDNA.reset();
+      return TypingDNA.recording = false;
+    }
+
+    /**
+     * This function outputs the typing pattern as a String, in a new basic structure for
+     * easy storage and usage in any kind of keystroke dynamics applications (e.g. typing
+     * pattern matching, user recognition)
+     * @param  {Number} length Optional: The amount of history keystrokes to use for the
+     * typing pattern. By default it will use the last 500 recorded keystrokes (or as many
+     * available if less than 500).
+     * @return {String}        The TypingDNA typing pattern, comma separated.
+     * A fixed vector of only numeric values separated by commas.
+     * @example var typingPattern = tdna.get();
+     * @example var typingPattern = tdna.get(200);
+     */
+    TypingDNA.get = function(length) {
+      var historyTotalLength = TypingDNA.history.stack.length;
+      if (length == undefined) {
+        length = TypingDNA.defaultHistoryLength;
+      }
+      if (length > historyTotalLength) {
+        length = historyTotalLength;
+      }
+      var obj = {};
+      obj.arr = TypingDNA.history.get(length);
+      var zl = TypingDNA.zl;
+      var histRev = length;
+      var histSktF = TypingDNA.math.fo(TypingDNA.history.get(length, "seek"));
+      var histPrtF = TypingDNA.math.fo(TypingDNA.history.get(length, "press"));
+      var histRevPF = histPrtF.length;
+      var histRevSF = histSktF.length;
+      var pressHistMean = Math.round(TypingDNA.math.avg(histPrtF));
+      var seekHistMean = Math.round(TypingDNA.math.avg(histSktF));
+      var pressHistSD = Math.round(TypingDNA.math.sd(histPrtF));
+      var seekHistSD = Math.round(TypingDNA.math.sd(histSktF));
+      var charMeanTime = seekHistMean + pressHistMean;
+      var pressRatio = TypingDNA.math.rd((pressHistMean + zl) / (charMeanTime + zl), 3);
+      var seekToPressRatio = TypingDNA.math.rd((1 - pressRatio)/pressRatio, 3);
+      var pressSDToPressRatio = TypingDNA.math.rd((pressHistSD + zl) / (pressHistMean + zl), 3);
+      var seekSDToPressRatio = TypingDNA.math.rd((seekHistSD + zl) / (pressHistMean + zl), 3);
+      var cpm = Math.round(6E4 / (charMeanTime + zl));
+      for (var i in obj.arr) {
+        var rev = obj.arr[i][1].length;
+        var seekMean = 0;
+        var pressMean = 0;
+        var postMean = 0;
+        var seekSD = 0;
+        var pressSD = 0;
+        var postSD = 0;
+        switch (obj.arr[i][0].length) {
+          case 0:
+            break;
+          case 1:
+            var seekMean = TypingDNA.math.rd((obj.arr[i][0][0] + zl) / (seekHistMean + zl), 3);
+            break;
+          default:
+            var arr = TypingDNA.math.fo(obj.arr[i][0]);
+            seekMean = TypingDNA.math.rd((TypingDNA.math.avg(arr) + zl) / (seekHistMean + zl), 3);
+            seekSD = TypingDNA.math.rd((TypingDNA.math.sd(arr) + zl) / (seekHistSD + zl), 3);
+        }
+        switch (obj.arr[i][1].length) {
+          case 0:
+            break;
+          case 1:
+            var pressMean = TypingDNA.math.rd((obj.arr[i][1][0] + zl) / (pressHistMean + zl), 3);
+            break;
+          default:
+            var arr = TypingDNA.math.fo(obj.arr[i][1]);
+            pressMean = TypingDNA.math.rd((TypingDNA.math.avg(arr) + zl) / (pressHistMean + zl), 3);
+            pressSD = TypingDNA.math.rd((TypingDNA.math.sd(arr) + zl) / (pressHistSD + zl), 3);
+        }
+        switch (obj.arr[i][2].length) {
+          case 0:
+            break;
+          case 1:
+            var postMean = TypingDNA.math.rd((obj.arr[i][2][0] + zl) / (seekHistMean + zl), 3);
+            break;
+          default:
+            var arr = TypingDNA.math.fo(obj.arr[i][2]);
+            postMean = TypingDNA.math.rd((TypingDNA.math.avg(arr) + zl) / (seekHistMean + zl), 3);
+            postSD = TypingDNA.math.rd((TypingDNA.math.sd(arr) + zl) / (seekHistSD + zl), 3);
+        }
+        delete obj.arr[i][2];
+        delete obj.arr[i][1];
+        delete obj.arr[i][0];
+        obj.arr[i][0] = rev;
+        obj.arr[i][1] = seekMean;
+        obj.arr[i][2] = pressMean;
+        obj.arr[i][3] = postMean;
+        obj.arr[i][4] = seekSD;
+        obj.arr[i][5] = pressSD;
+        obj.arr[i][6] = postSD;
+      }
+      var arr = [];
+      TypingDNA.apu(arr, histRev);
+      TypingDNA.apu(arr, cpm);
+      TypingDNA.apu(arr, charMeanTime);
+      TypingDNA.apu(arr, pressRatio);
+      TypingDNA.apu(arr, seekToPressRatio);
+      TypingDNA.apu(arr, pressSDToPressRatio);
+      TypingDNA.apu(arr, seekSDToPressRatio);
+      TypingDNA.apu(arr, pressHistMean);
+      TypingDNA.apu(arr, seekHistMean);
+      TypingDNA.apu(arr, pressHistSD);
+      TypingDNA.apu(arr, seekHistSD);
+      for (var c = 0; c <= 6; c++) {
+        for (var i = 0; i < 44; i++) {
+          var keyCode = TypingDNA.chars[i];
+          var val = obj.arr[keyCode][c];
+          if (val == 0 && c > 0) {
+            val = 1;
+          }
+          TypingDNA.apu(arr, val);
+        }
+      }
+      return arr.join(",");
+    }
+    TypingDNA.apu = function(arr, val) {
+      "NaN" == String(val) && (val = 0);
+      arr.push(val);
+    }
+    TypingDNA.math = {};
+    TypingDNA.math.rd = function(val, dec) {
+      return Number(val.toFixed(dec));
+    }
+    TypingDNA.math.avg = function(arr) {
+      var len = arr.length;
+      var sum = 0;
+      for (var i = 0; i < len; i++) {
+        sum += arr[i];
+      }
+      return this.rd(sum / len, 3);
+    }
+    TypingDNA.math.sd = function(arr) {
+      var len = arr.length;
+      if (len < 2) {
+        return 0;
+      } else {
+        var sumVS = 0;
+        var mean = this.avg(arr);
+        for (var i = 0; i < len; i++) {
+          sumVS += (arr[i] - mean) * (arr[i] - mean);
+        }
+        var sd = Math.sqrt(sumVS / len);
+        return sd;
+      }
+    }
+    TypingDNA.math.fo = function(arr) {
+      var values = arr.concat();
+      values.sort(function(a, b) {
+        return a - b;
+      });
+      var asd = this.sd(values);
+      var aMean = values[Math.ceil(arr.length / 2)];
+      var multiplier = 2;
+      var maxVal = aMean + multiplier * asd;
+      var minVal = aMean - multiplier * asd;
+      if (arr.length < 20) {
+        minVal = 0;
+      }
+      var fVal = values.filter(function(x) {
+        return x < maxVal && x > minVal;
+      });
+      return fVal;
+    }
+    TypingDNA.history = {};
+    TypingDNA.history.stack = [];
+    TypingDNA.history.add = function(arr) {
+      this.stack.push(arr);
+      if (this.stack.length > TypingDNA.maxHistoryLength) {
+        this.stack.shift();
+      }
+    }
+    TypingDNA.history.get = function(length, type) {
+      var historyTotalLength = this.stack.length;
+      if (length == 0 || length == undefined) {
+        length = TypingDNA.defaultHistoryLength;
+      }
+      if (length > historyTotalLength) {
+        length = historyTotalLength;
+      }
+      switch (type) {
+        case "seek":
+          var seekArr = [];
+          for (i = 1; i <= length; i++) {
+            var seekTime = this.stack[historyTotalLength - i][1];
+            if (seekTime <= TypingDNA.maxSeekTime) {
+              seekArr.push(seekTime);
+            }
+          };
+          return seekArr;
+          break;
+        case "press":
+          var pressArr = [];
+          for (i = 1; i <= length; i++) {
+            var pressTime = this.stack[historyTotalLength - i][2];
+            if (pressTime <= TypingDNA.maxPressTime) {
+              pressArr.push(pressTime);
+            }
+          };
+          return pressArr;
+          break;
+        default:
+          var historyStackObj = {};
+          for (var i in TypingDNA.chars) {
+            historyStackObj[TypingDNA.chars[i]] = [
+              [],
+              [],
+              []
+            ];
+          }
+          for (i = 1; i <= length; i++) {
+            var arr = this.stack[historyTotalLength - i];
+            var keyCode = arr[0];
+            var seekTime = arr[1];
+            var pressTime = arr[2];
+            var prevKeyCode = arr[3];
+            if (TypingDNA.chars.indexOf(keyCode) != -1) {
+              if (seekTime <= TypingDNA.maxSeekTime) {
+                historyStackObj[keyCode][0].push(seekTime);
+                if (prevKeyCode != 0 && TypingDNA.chars.indexOf(prevKeyCode) != -1) {
+                  historyStackObj[prevKeyCode][2].push(seekTime);
+                }
+              }
+              if (pressTime <= TypingDNA.maxPressTime) {
+                historyStackObj[keyCode][1].push(pressTime);
+              }
+            }
+          };
+          return historyStackObj;
+      }
+    }
+  }
+
+  /**
+   * Checks the quality of a typing pattern, how well it is revelated, how useful the
+   * information will be for matching applications. It returns a value between 0 and 1.
+   * Values over 0.3 are acceptable, however a value over 0.7 shows good pattern strength.
+   * @param  {String} typingPattern The typing pattern string returned by the get() function.
+   * @return {Number} A real number between 0 and 1. A close to 1 value means a stronger pattern.
+   * @example var quality = tdna.getQuality(typingPattern);
+   */
+  TypingDNA.getQuality = function(typingPattern) {
+    var obj = typingPattern.split(",").map(Number);
+    var totalEvents = obj[0];
+    var acc = rec = avgAcc = 0;
+    var avg = TypingDNA.math.avg(obj);
+    var revs = obj.slice(11, 55);
+    for (var i in revs) {
+      rec += Number(revs[i] > 0);
+      acc += Number(revs[i] > 4);
+      avgAcc += Number(revs[i] > avg);
+    }
+    var tReturn = Math.sqrt(rec * acc * avgAcc) / 80;
+    return tReturn > 1 ? 1 : tReturn;
+  }
+
+}
